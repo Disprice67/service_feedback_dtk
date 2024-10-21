2dd920327f0f579e479fc84418070783ab87ca24
Дмитрий Уткин <dimon965316@yandex.ru>
2024-10-21 12:06:25 +0300

    version 1.0

--- adfs/adfs_middleware.py
+++ adfs/adfs_middleware.py
@@ -7,7 +7,6 @@ class RedirectToLoginMiddleware:
 
     def __call__(self, request):
         allowed_paths = ['/sso/login/', '/acs/', '/sso/logout/', '/metadata/', '/djadmin/', '/djadmin/login/']
-        print(request.path)
         if not request.user.is_authenticated and request.path not in allowed_paths and '/api/' not in request.path:
             return redirect('/sso/login/')
         return self.get_response(request)
